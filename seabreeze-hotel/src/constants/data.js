5bd93569f750ad0e3469367438b59c6ebc9c8737
AsraAnvar <107696047+AsraAnvar@users.noreply.github.com>
2023-07-17 20:24:07 +0530

    initcommit

---
+++ seabreeze-hotel/src/constants/data.js
@@ -0,0 +1,82 @@
+import images from './images';
+
+const wines = [
+  {
+    title: 'Chapel Hill Shiraz',
+    price: '$56',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'Catena Malbee',
+    price: '$59',
+    tags: 'AU | Bottle',
+  },
+  {
+    title: 'La Vieillw Rose',
+    price: '$44',
+    tags: 'FR | 750 ml',
+  },
+  {
+    title: 'Rhino Pale Ale',
+    price: '$31',
+    tags: 'CA | 750 ml',
+  },
+  {
+    title: 'Irish Guinness',
+    price: '$26',
+    tags: 'IE | 750 ml',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Aperol Sprtiz',
+    price: '$20',
+    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
+  },
+  {
+    title: "Dark 'N' Stormy",
+    price: '$16',
+    tags: 'Dark rum | Ginger beer | Slice of lime',
+  },
+  {
+    title: 'Daiquiri',
+    price: '$10',
+    tags: 'Rum | Citrus juice | Sugar',
+  },
+  {
+    title: 'Old Fashioned',
+    price: '$31',
+    tags: 'Bourbon | Brown sugar | Angostura Bitters',
+  },
+  {
+    title: 'Negroni',
+    price: '$26',
+    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.feature01,
+    title: 'Receptionist',
+    subtitle: 'Your Welcoming Guide: Meet Our Friendly Front Desk Team.',
+  },
+  {
+    imgUrl: images.feature02,
+    title: 'Morning Tea',
+    subtitle: 'Awaken Your Senses: Enjoy Our Complimentary Morning Tea.',
+  },
+  {
+    imgUrl: images.feature03,
+    title: 'Luggage',
+    subtitle: 'Travel with Ease: Let Us Handle Your Luggage Hassle.',
+  },
+  {
+    imgUrl: images.feature04,
+    title: 'Playground',
+    subtitle: 'Fun & Adventure for All Ages: Discover Our Playful Playground.',
+  },
+];
+
+export default { wines, cocktails, features };
